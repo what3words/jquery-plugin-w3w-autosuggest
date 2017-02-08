acfce85ad51f06197537ecbad17f91f6cdb6b117
Josh Francovszky <franszo@codetheory.london>
2017-02-08 11:39:39 +0000

    removed gulp tasks

--- gulp/tasks/images.js
+++ gulp/tasks/images.js
@@ -6,14 +6,3 @@ gulp.task('images', ['svg']);
 gulp.task('svg', function() {
     return gulp.src('src/images/**/*.*').pipe(gulp.dest('dist/images/'));
 });
-
-// gulp.task('tinypng', function() {
-//     return gulp.src('./dist/images/**/*.{png,jpg,jpeg}')
-//         .pipe(changed('./src/images/'))
-//         .pipe(tinypng({
-//             key: 'KtQpQekBSuPe164oHFgL4g8gtMmxRdjt',
-//             sigFile: './src/images/.tinypng-sigs',
-//             log: true
-//         }))
-//         .pipe(gulp.dest('./dist/images/'));
-// });
--- gulp/tasks/local.config.sample.js
+++
@@ -1,9 +0,0 @@
-// localConfig
-var localConfig = {
-  remote: {
-    host: 'beta.dev.what3words.com',
-    user: '',
-    dest: '/var/www/demo/checkout'
-  }
-};
-module.exports = localConfig;
