f1fd238b5430bc89a93caecdf9fb553f0d336316
arnaud ferrand <arferrand@gmail.com>
2017-03-08 16:09:15 +0000

    links uglify with lint to prevent breaking error (especially with watch)

--- gulp/tasks/js-build.js
+++ gulp/tasks/js-build.js
@@ -4,37 +4,37 @@ var rename = require('gulp-rename');
 var uglify = require('gulp-uglify');
 var sourcemaps = require('gulp-sourcemaps');
 
-gulp.task('js-build', ['lint', 'jsminify', 'js-bundle-min']);
+gulp.task('js-build', ['jsminify', 'js-bundle-min']);
 
-gulp.task('jsminify', function () {
-  return gulp.src(['src/js/jquery.w3w-autosuggest-plugin.js'])
+var minify = function (source, destination) {
+  return gulp.src(source)
     .pipe(sourcemaps.init())
     .pipe(gulp.dest('dist/js'))
-    .pipe(rename('jquery.w3w-autosuggest-plugin.min.js'))
+    .pipe(rename(destination))
     .pipe(uglify({
       outSourceMap: true
     }))
     .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('dist/js'));
+};
+
+gulp.task('jsminify', ['lint'], function () {
+  return minify('src/js/jquery.w3w-autosuggest-plugin.js',
+    'jquery.w3w-autosuggest-plugin.min.js');
 });
 
 gulp.task('js-bundle-min', ['js-bundle'], function () {
-  return gulp.src('dist/js/jquery.w3w-autosuggest-plugin.bundle.js')
-  .pipe(sourcemaps.init())
-  .pipe(gulp.dest('dist/js'))
-  .pipe(rename('jquery.w3w-autosuggest-plugin.bundle.min.js'))
-  .pipe(uglify({
-    outSourceMap: true
-  }))
-  .pipe(sourcemaps.write('.'))
-  .pipe(gulp.dest('dist/js'));
+  return minify('dist/js/jquery.w3w-autosuggest-plugin.bundle.js',
+  'jquery.w3w-autosuggest-plugin.bundle.min.js');
 });
 
-gulp.task('js-bundle', function () {
-  return gulp.src(['src/js/jquery.w3w-autosuggest-plugin.js',
+gulp.task('js-bundle', ['lint'], function () {
+  var sources = [
+    'src/js/jquery.w3w-autosuggest-plugin.js',
     'node_modules/jquery-typeahead/dist/jquery.typeahead.min.js',
     'node_modules/jquery-validation/dist/jquery.validate.min.js'
-  ])
-  .pipe(concat('jquery.w3w-autosuggest-plugin.bundle.js'))
-  .pipe(gulp.dest('dist/js'));
+  ];
+  return gulp.src(sources)
+    .pipe(concat('jquery.w3w-autosuggest-plugin.bundle.js'))
+    .pipe(gulp.dest('dist/js'));
 });
